1d3fe5f1a8c8d247f3e8dfc6154449ec0bba2157
Vladimir Petrov <vppetrov@mm-sol.com>
2012-10-26 17:19:35 +0300

    SurfaceTexture: Fixed build without CPCAM
    Fixes broken build caused by inproper macro
    introduced with with change I66d6b538.
    
    Change-Id: Ib69c1923ea83109fa135ea493d434cc7c0a43da7
    Signed-off-by: Vladimir Petrov <vppetrov@mm-sol.com>

--- libs/gui/BufferQueue.cpp
+++ libs/gui/BufferQueue.cpp
@@ -229,7 +229,7 @@ int BufferQueue::query(int what, int* outValue)
     case NATIVE_WINDOW_CONSUMER_RUNNING_BEHIND:
         value = (mQueue.size() >= 2);
         break;
-#ifdef OMAP_ENHANCEMENT
+#ifdef OMAP_ENHANCEMENT_CPCAM
     case NATIVE_WINDOW_BUFFER_COUNT:
         value = mMaxAcquiredBufferCount;
         break;
