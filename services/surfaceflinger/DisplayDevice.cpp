b39ab1bcaa79cc94298c19384bf9adb79ba546ed
Wangbaoyun <wangbaoyun@hhcn.com>
2013-04-22 17:42:23 +0800

    change view mode of virtual display from orthon to perspective
    Signed-off-by: Wangbaoyun <wangbaoyun@hhcn.com>

--- services/surfaceflinger/DisplayDevice.cpp
+++ services/surfaceflinger/DisplayDevice.cpp
@@ -278,14 +278,88 @@ EGLBoolean DisplayDevice::makeCurrent(EGLDisplay dpy,
     return result;
 }
 
+void gluPerspective( float fovy, float aspect, float near_clip, float far_clip )
+{
+    const double PI = 3.1415926;
+    double TWOPI_OVER_360 = 2.0 * PI / 360.0;
+    float half_height = near_clip * (float)tan( fovy * 0.5 * TWOPI_OVER_360 );
+    float half_width = half_height * aspect;
+
+    glFrustumf( -half_width, half_width, -half_height, half_height, near_clip, far_clip );
+}
+
+void gluLookAt( float eyex, float eyey, float eyez,
+        float centerx, float centery, float
+        centerz,
+        float upx, float upy, float upz )
+{
+    float m[16];
+    float x[3], y[3], z[3];
+    float mag;
+    /* Make rotation matrix */
+    /* Z vector */
+    z[0] = eyex - centerx;
+    z[1] = eyey - centery;
+    z[2] = eyez - centerz;
+    mag = sqrt( z[0]*z[0] + z[1]*z[1] + z[2]*z[2] );
+    if (mag) {  /* mpichler, 19950515 */
+        z[0] /= mag;
+        z[1] /= mag;
+        z[2] /= mag;
+    }
+    /* Y vector */
+    y[0] = upx;
+    y[1] = upy;
+    y[2] = upz;
+    /* X vector = Y cross Z */
+    x[0] =  y[1]*z[2] - y[2]*z[1];
+    x[1] = -y[0]*z[2] + y[2]*z[0];
+    x[2] =  y[0]*z[1] - y[1]*z[0];
+    /* Recompute Y = Z cross X */
+    y[0] =  z[1]*x[2] - z[2]*x[1];
+    y[1] = -z[0]*x[2] + z[2]*x[0];
+    y[2] =  z[0]*x[1] - z[1]*x[0];
+    /* mpichler, 19950515 */
+    /* cross product gives area of parallelogram, which is < 1.0 for
+     * non-perpendicular unit-length vectors; so normalize x, y here
+     */
+    mag = sqrt( x[0]*x[0] + x[1]*x[1] + x[2]*x[2] );
+    if (mag) {
+        x[0] /= mag;
+        x[1] /= mag;
+        x[2] /= mag;
+    }
+    mag = sqrt( y[0]*y[0] + y[1]*y[1] + y[2]*y[2] );
+    if (mag) {
+        y[0] /= mag;
+        y[1] /= mag;
+        y[2] /= mag;
+    }
+#define M(row,col)  m[col*4+row]
+    M(0,0) = x[0];  M(0,1) = x[1];  M(0,2) = x[2];  M(0,3) = 0.0;
+    M(1,0) = y[0];  M(1,1) = y[1];  M(1,2) = y[2];  M(1,3) = 0.0;
+    M(2,0) = z[0];  M(2,1) = z[1];  M(2,2) = z[2];  M(2,3) = 0.0;
+    M(3,0) = 0.0;   M(3,1) = 0.0;   M(3,2) = 0.0;   M(3,3) = 1.0;
+#undef M
+    glMultMatrixf( m );
+    /* Translate Eye to Origin */
+    glTranslatef( -eyex, -eyey, -eyez );
+}
+
 void DisplayDevice::setViewportAndProjection(const sp<const DisplayDevice>& hw) {
     GLsizei w = hw->mDisplayWidth;
     GLsizei h = hw->mDisplayHeight;
-    glViewport(0, 0, w, h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    // put the origin in the left-bottom corner
-    glOrthof(0, w, 0, h, -w, w); // l=0, r=w ; b=0, t=h
+    if(hw->getDisplayType()==2) {
+        glViewport(0, 0, w, h);
+        gluPerspective(45, float(w)/float(h), 1, 1000);
+        gluLookAt( w/2, h/2, 580.0, w/2, h/2, 0.0, 0.0, 1.0, 0.0 );
+    } else {
+        // put the origin in the left-bottom corner
+        glViewport(0, 0, w, h);
+        glOrthof(0, w, 0, h, -w, w); // l=0, r=w ; b=0, t=h
+    }
     glMatrixMode(GL_MODELVIEW);
 }
 
